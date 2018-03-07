0097deac27992b41d29678351ba5d2eb65ca2717
Francesco Benintende <francescobenintende9@gmail.com>
2018-03-07 10:32:12 +0000

    Fixed simulation bug

--- src/individual.py
+++ src/individual.py
@@ -6,28 +6,9 @@ class Individual():
         self.partner = None
 
     def get_priority(self, other):
-        """Get the priority of a match from the perspective of the current
-        person.
-
-        Args:
-            other: The other person in the prospective match.
-
-        Returns:
-            An index indicating the priority of a match.
-            Lower numbers indicate higher priorities.
-        """
         return self.preference_list.index(other)
 
-    def get_id_list(self, instance_list):
-        """Get a list of ID numbers corresponding to the input
-        list of instances.
-
-        Args:
-            intance_list: A list of other Individual objects.
-
-        Returns:
-            A list of ID numbers.
-        """
+    def get_list_id(self, instance_list):
         id_list = []
         for i in instance_list:
             id_list.append(i.id_number if i else None)
@@ -36,8 +17,8 @@ class Individual():
     def __str__(self):
         output = ('id_number={0} preference_list={1} available_proposals={2} '
                   'partner={3}'.format(
-                     self.id_number,
-                     self.get_id_list(self.preference_list),
-                     self.get_id_list(self.available_proposals),
-                     self.partner.id_number if self.partner else None))
+                      self.id_number,
+                      self.get_list_id(self.preference_list),
+                      self.get_list_id(self.available_proposals),
+                      self.partner.id_number if self.partner else None))
         return output
