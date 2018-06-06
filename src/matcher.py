6ff7cc1c82a3ff1371db05ed7e700f1de9fcb367
carlocamurri <carlo.camurri98@gmail.com>
2018-06-06 01:36:58 +0100

    Encapsulation Matcher class

--- src/matcher.py
+++ src/matcher.py
@@ -1,16 +1,21 @@
-def match(students):
-    for student in students:
-        student.project = student.project_priorities[0]
+class Matcher:
 
-def is_stable(students):
-    for student in students:
-        if not student.is_matched():
-            return False
-    return True
+    def __init__(self, students):
+        self.students = students
 
-def get_available_projects(students):
-    available_projects = set(students[0].project_priorities)
-    for student in students:
-        if student.is_matched():
-            available_projects.remove(student.project)
-    return available_projects
+    def match(self):
+        for student in self.students:
+            student.project = student.project_priorities[0]
+
+    def is_stable(self):
+        for student in self.students:
+            if not student.is_matched():
+                return False
+        return True
+
+    def get_available_projects(self):
+        available_projects = set(self.students[0].project_priorities)
+        for student in self.students:
+            if student.is_matched():
+                available_projects.remove(student.project)
+        return available_projects
--- tests/test_matcher.py
+++ tests/test_matcher.py
@@ -1,6 +1,6 @@
 import unittest
 
-import src.matcher as matcher
+from src.matcher import Matcher
 from src.student import Student
 
 
@@ -8,14 +8,16 @@ class MatcherTest(unittest.TestCase):
 
     def test_matches_one_student_and_one_project(self):
         student = Student(1, [1])
-        matcher.match([student])
+        matcher = Matcher([student])
+        matcher.match()
         self.assertEqual(student.project, 1)
 
     def test_each_student_is_prioritized(self):
         student_1 = Student(1, [1, 2, 3])
         student_2 = Student(2, [2, 1, 3])
         student_3 = Student(3, [3, 1, 2])
-        matcher.match([student_1, student_2, student_3])
+        matcher = Matcher([student_1, student_2, student_3])
+        matcher.match()
         self.assertEqual(student_1.project, 1)
         self.assertEqual(student_2.project, 2)
         self.assertEqual(student_3.project, 3)
@@ -24,7 +26,8 @@ class MatcherTest(unittest.TestCase):
     def test_student_with_highest_grade_is_prioritized(self):
         student_1 = Student(1, [1, 2], grade=10)
         student_2 = Student(2, [1, 2], grade=7)
-        matcher.match([student_1, student_2])
+        matcher = Matcher([student_1, student_2])
+        matcher.match()
         self.assertEqual(student_1.project, 1)
         self.assertEqual(student_1.project, 2)
     '''
@@ -33,28 +36,33 @@ class MatcherTest(unittest.TestCase):
         student_1 = Student(1, [1, 2])
         student_2 = Student(2, [2, 1])
         students = [student_1, student_2]
-        matcher.match(students)
-        self.assertTrue(matcher.is_stable(students))
+        matcher = Matcher(students)
+        matcher.match()
+        self.assertTrue(matcher.is_stable())
 
     def test_some_unmatched_is_unstable(self):
         student_1 = Student(1, [1, 2])
         student_2 = Student(2, [2, 1])
         students = [student_1, student_2]
-        self.assertFalse(matcher.is_stable(students))
+        matcher = Matcher(students)
+        self.assertFalse(matcher.is_stable())
 
     def test_some_matched_none_is_unstable(self):
         student_1 = Student(1, [1, 2])
         student_2 = Student(2, [2, 1])
         student_1.project = None
         students = [student_1, student_2]
-        self.assertFalse(matcher.is_stable(students))
+        matcher = Matcher(students)
+        self.assertFalse(matcher.is_stable())
 
     def test_all_projects_are_available_at_beginning(self):
         student_1 = Student(1, [1, 2, 3])
         student_2 = Student(2, [2, 1, 3])
         student_3 = Student(3, [3, 1, 2])
+        students = [student_1, student_2, student_3]
+        matcher = Matcher(students)
         self.assertEqual( \
-            matcher.get_available_projects([student_1, student_2, student_3]), \
+            matcher.get_available_projects(), \
             set([1, 2, 3]) \
         )
 
@@ -63,7 +71,9 @@ class MatcherTest(unittest.TestCase):
         student_2 = Student(2, [2, 1, 3])
         student_3 = Student(3, [3, 1, 2])
         student_2.project = 2
+        students = [student_1, student_2, student_3]
+        matcher = Matcher(students)
         self.assertEqual( \
-            matcher.get_available_projects([student_1, student_2, student_3]), \
+            matcher.get_available_projects(), \
             set([1, 3]) \
         )
