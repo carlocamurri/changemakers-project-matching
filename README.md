11b6e2b82376477265e12dce871fcb42e6357c0a
Francesco Benintende <francescobenintende9@gmail.com>
2018-07-19 12:22:58 +0200

    Renaming and updating README

--- README.md
+++ README.md
@@ -2,8 +2,18 @@
 UCL Moodle Extension based on the stable marriage algorithm to manage project assingments based on preferences.
 
 # HOW TO RUN
-Browse into src, then run the python script passing as parameter the population size for the simulation.
+Browse into the folder containing changemakers-project-matching and run:
 ```
-cd src
-python run.py 10
+virtualenv changemakers-project-matching
+```
+Then:
+```
+cd changemakers-project-matching
+source bin/activate
+```
+
+From here you can run a simulation of usage by browsing in the simulation folder and running sim.py
+```
+cd sim
+python sim.py
 ```
--- sim/sim.py
+++ sim/sim.py
@@ -1,37 +1,46 @@
+from src.matcher import Matcher
+from src.entities.student import Student
+from src.entities.project import Project
+
 users = {
-    "carlocamurri" : "bonfire",
-    "francescobenintende" : "UCLove<3",
-    "miquel" : "crossfit"
+    "16077909" : "Password1",
+    "16977910" : "Password2"
 }
 
 def intro():
     print "\n- - - UCL CHANGEMAKERS - - -"
-    print "Simulation of workflow"
+    print "- - Simulation of workflow - -"
     print "------------------------------\n"
 
 def insert_credential():
-    username = raw_input('username: ')
-    password = raw_input("password: ")
+    username = raw_input('User ID: ')
+    password = raw_input("Password: ")
     validate_account(username, password)
 
 def login():
     username = ""
     password = ""
-    print "Enter login credentials"
+    print "Enter login credentials."
     insert_credential()
     print username
 
 def validate_account(username, password): 
     if username not in users:
         print "Unregistred Account!\n"
+        username = raw_input("Re-enter username: ")
+        validate_account(username, password)
     else:    
         if users[username] == password:
-            print "\nSuccessful login"
+            print "\nSuccessful login!"
         else: 
             print "Invalid password\n"
             password = raw_input("Re-enter password: ")
             validate_account(username, password)
 
+# def load_data():
+
 
 intro()
 login()
+# load_data()
+
